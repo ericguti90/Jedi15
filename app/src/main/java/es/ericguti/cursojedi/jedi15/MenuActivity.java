7f4aca0713235aa572cdb889a9a23bd6109fbe28
ericguti.90 <ericguti.90@gmail.com>
2015-07-14 17:34:01 +0200

    v0.1 FAB

--- app/src/main/java/es/ericguti/cursojedi/jedi15/MenuActivity.java
+++ app/src/main/java/es/ericguti/cursojedi/jedi15/MenuActivity.java
@@ -3,12 +3,18 @@ package es.ericguti.cursojedi.jedi15;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
 
 
 public class MenuActivity extends ActionBarActivity implements View.OnClickListener {
@@ -54,6 +60,7 @@ public class MenuActivity extends ActionBarActivity implements View.OnClickListe
         rank.setOnClickListener(this);
         user.setOnClickListener(this);
         play.setOnClickListener(this);
+        createB();
     }
 
     @Override
@@ -84,5 +91,32 @@ public class MenuActivity extends ActionBarActivity implements View.OnClickListe
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    void createB(){
+
+        final View actionB = findViewById(R.id.action_b);
+
+        FloatingActionButton actionC = new FloatingActionButton(getBaseContext());
+        //actionC.setTitle("Hide/Show Action above");
+        //actionC.setOnClickListener(new View.OnClickListener() {
+        //   @Override
+        //    public void onClick(View v) {
+        //        actionB.setVisibility(actionB.getVisibility() == View.GONE ? View.VISIBLE : View.GONE);
+        //    }
+        //});
+        //((FloatingActionsMenu) findViewById(R.id.multiple_actions)).addButton(actionC);
+
+
+
+
+        final FloatingActionButton actionA = (FloatingActionButton) findViewById(R.id.action_a);
+        actionA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                actionA.setTitle("Action A clicked");
+            }
+        });
     }
 }
